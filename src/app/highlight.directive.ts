b07ed143906db0b5af97fece2439a933939e0954
janenjihia <jane.njihia@student.moringaschool.com>
2022-04-11 11:25:10 +0300

    add functions on class quote,pipe.ts

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,21 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef,HostListener} from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  constructor( private elem: ElementRef) { 
+    elem.nativeElement.style.backgroundcolor = "blue"
+  }
+  @HostListener("click") onClicks(){
+    this.textDeco("red")
+  }
 
+  @HostListener("dblclick") onDoubleClicks(){
+    this.textDeco("None")
+  }
+  private textDeco(action:string){
+    this.elem.nativeElement.style.color=action;
+  }
 }
