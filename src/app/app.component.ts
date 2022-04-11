70ab970d1fc8eb9641e0e771e21a08f47ad7338b
janenjihia <jane.njihia@student.moringaschool.com>
2022-04-11 11:27:09 +0300

    add add new quote function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'quoz';
+  quote: any;
+
+  addNewQuote(quote: any) {
+    let quoteLength = this.quote.length;
+    quote.id = quoteLength + 1;
+    quote.postDate = new Date(quote.postDate)
+    this.quote.push(quote);
+  }
+  // title = 'quotes';
 }
