920e29a81799514d51eb786f1afb9d2a807330ab
janenjihia <jane.njihia@student.moringaschool.com>
2022-04-11 11:26:17 +0300

    modify app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 import { QuotesComponent } from './quotes/quotes.component';
-import { QuoteFormComponent } from './quote-form/quote-form.component';
-import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
-import { DatePipe } from './date.pipe';
 import { HighlightDirective } from './highlight.directive';
+import { QuoteDetailsComponent } from './quote-detail/quote-detail.component';
+import { QuotesFormComponent } from './quote-form/quote-form.component';
+
+import { DatePipePipe } from './date-pipe.pipe';
+import { FormsModule, NgForm, NgModel } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
     QuotesComponent,
-    QuoteFormComponent,
-    QuoteDetailComponent,
-    DatePipe,
-    HighlightDirective
+    HighlightDirective,
+    DatePipePipe, 
+    QuoteDetailsComponent,
+    QuotesFormComponent,
+    
+
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // NgForm,
+    // NgModel,
+    FormsModule,
+    // ReactiveFormsModule,
+   
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
